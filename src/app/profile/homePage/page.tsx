a186cb11493aaf1c32a2db909040ca4e0e99ce80
Xihao Zhu <xihao.zhu@outlook.com>
2024-05-29 05:39:31 +0100

    problem fixed and links added

--- src/app/profile/homePage/page.tsx
+++ src/app/profile/homePage/page.tsx
@@ -1,5 +1,5 @@
 export default function HomePage() {
   return (<>
-    <div className="bg-[url('@/../../public/pics/nickPolkaDot.png')] w-72 h-72 bg-contain rounded-full"></div>
+    <div className="bg-[url('/pics/nickPolkaDot.png')] w-72 h-72 bg-contain rounded-full"></div>
     </>);
 }
--- src/app/ui/headNav.tsx
+++ src/app/ui/headNav.tsx
@@ -3,42 +3,46 @@
 import './globals.css'
 import clsx from 'clsx'
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function HeadNav(){
   
   const pathname = usePathname();
 
   return (<>
-    <div className='mx-auto flex justify-around p-4 flex-wrap align-middle bg-color1 my_blur'>
+    <div className={clsx('mx-auto flex justify-around p-4 flex-wrap align-middle bg-color1 lg:hidden',{
+      'hidden':pathname !== '/profile/homePage'
+    })}>
       {/* logo when in small and middle screen, take a single row */}
-      <div className='lg:hidden w-32 mb-4 h-16 bg-[url("@/../../public/pics/logo.png")] bg-contain'></div>
+      <div className='w-32 mb-4 h-16 bg-[url("/pics/logo.png")] bg-contain'></div>
       {/* navi buttons for small and middle screen */}
-      <ol className='lg:hidden flex w-11/12 justify-between lg:w-4/6 flex-wrap lg:flex-nowrap font-bold'>
+      <ol className='flex w-11/12 justify-between lg:w-4/6 flex-wrap font-bold'>
         {/* <li className=
           'bg-color2 text-color3 w-full lg:w-1/6 mb-2 lg:mb-0 flex items-center justify-center'
         >
           <span className="material-symbols-outlined mr-2">home</span> home page
         </li> */}
 
-        <li className='bg-color2 text-color3 w-full lg:w-1/6 mb-2 lg:mb-0 flex items-center justify-center rounded-md'>
+        <Link href={'/profile/aboutMe'} className='bg-color2 text-color3 w-full lg:w-1/6 mb-2 lg:mb-0 flex items-center justify-center rounded-md'>
           <span className="material-symbols-outlined mr-2">person</span>about me
-        </li>
+        </Link>
 
-        <li className='bg-color2 text-color3 w-full lg:w-1/6 mb-2 lg:mb-0 flex items-center justify-center rounded-md'>
+        <Link href={'/profile/skills'} className='bg-color2 text-color3 w-full lg:w-1/6 mb-2 lg:mb-0 flex items-center justify-center rounded-md'>
           <span className="material-symbols-outlined mr-2">laptop_windows</span>skills
-        </li>
+        </Link>
 
-        <li className='bg-color2 text-color3 w-full lg:w-1/6 flex items-center justify-center rounded-md'>
+        <Link href={'/profile/contactMe'} className='bg-color2 text-color3 w-full lg:w-1/6 flex items-center justify-center rounded-md'>
           <span className="material-symbols-outlined mr-2">mail</span>contanct me
-        </li>
+        </Link>
       </ol>
+    </div>
 
-
+    <div className='mx-auto justify-around p-4 flex-wrap align-middle bg-color1 hidden lg:flex'> 
       {/* logo in big screen */}
-      <div className='w-40 hidden lg:flex h-20 bg-[url("@/../../public/pics/logo.png")] bg-contain'></div>
+      <div className='w-40 hidden lg:flex h-20 bg-[url("/pics/logo.png")] bg-contain'></div>
       {/* navi buttons for large screens */}
       <ol className='hidden lg:flex w-11/12 justify-between lg:w-4/6 flex-wrap lg:flex-nowrap font-bold'>
-        <li className={clsx(
+        <Link href={'/profile/homePage'} className={clsx(
           'text-color3 w-1/6 flex items-center justify-center hover:bg-color3 hover:text-color4 rounded-lg',
           {
             'text-yellow-500':pathname === '/profile/homePage'
@@ -46,31 +50,31 @@ export default function HeadNav(){
         )}>
           <span className="material-symbols-outlined mr-4">home</span>
           home page
-        </li>
-        <li className={clsx(
+        </Link>
+        <Link href={'/profile/aboutMe'} className={clsx(
           'text-color3 w-1/6 flex items-center justify-center hover:bg-color3 hover:text-color4 rounded-lg',
           {
             'text-yellow-500':pathname === '/profile/aboutMe'
           }
         )}>
           <span className="material-symbols-outlined mr-4">person</span>about me
-        </li>
-        <li className={clsx(
+        </Link>
+        <Link href={'/profile/skills'} className={clsx(
           'text-color3 w-1/6 flex items-center justify-center hover:bg-color3 hover:text-color4 rounded-lg',
           {
             'text-yellow-500':pathname === '/profile/skills'
           }
         )}>
           <span className="material-symbols-outlined mr-4">laptop_windows</span>skills
-        </li>
-        <li className={clsx(
+        </Link>
+        <Link href={'/profile/contactMe'} className={clsx(
           'text-color3 w-1/6 flex items-center justify-center hover:bg-color3 hover:text-color4 rounded-lg',
           {
             'text-yellow-500':pathname === '/profile/contactMe'
           }
         )}>
           <span className="material-symbols-outlined mr-4">mail</span>contanct me
-        </li>
+        </Link>
       </ol>
     </div>
   </>)
