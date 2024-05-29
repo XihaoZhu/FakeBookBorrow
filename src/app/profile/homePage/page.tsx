9c8689fabcbe9d07d65e77995762bed78dbd935c
Xihao Zhu <xihao.zhu@outlook.com>
2024-05-29 20:28:02 +0100

    typing animation finihsed

--- src/app/profile/homePage/page.tsx
+++ src/app/profile/homePage/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Typing from "@/app/ui/typing";
 
 export default function HomePage() {
   return (<>
@@ -6,9 +7,20 @@ export default function HomePage() {
     {/* for large and above screen */}
     <div className="w-full h-full hidden lg:flex flex-row px-[10vw]">
       {/* the text part */}
-      <div className="flex-1">
-        <div>Hello I&apos;m Nick</div>
+      <div className="flex-1 flex flex-col justify-center text-center">
+        {/* the title part */}
+        <div>
+          {/* fixed part */}
+          <div className="text-7xl">Hello, I&apos;m</div>
+          {/* moving part */}
+          <div className="text-7xl">
+          {/* <Typing string1="Nick" string2="Xihao Zhu"/> */}
+          <div>Xihao Zhu|</div>
+          </div>
+        </div>
+        {/* description */}
         <div>Here will be a short description</div>
+        {/* link to download cv */}
         <div>here will be a botton to download my cv</div>
       </div>
       {/* my big head */}
---
+++ src/app/ui/typing.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { randomInt } from "crypto"
+import React from "react"
+
+export default function Typing(props:{string1:string,string2:string}={string1:'string1',string2:'string2'}){
+  
+  const {string1,string2} = props
+  const [text,setText] = React.useState('')
+  const [currentType,setCurrentType] = React.useState(string1)
+  const delay = (ms:number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  const type = async () => {
+    for (let i = 0; i < currentType.length; i++) {
+      await delay(Math.random()*500+100); 
+      setText(currentType.slice(0, i + 1));
+    }
+    await delay(300)
+    for (let i = currentType.length; i > 0; i--) {
+      await delay(Math.random()*500+100);
+      setText(currentType.slice(0, i - 1));
+    }
+    setCurrentType(currentType === string1 ? string2 : string1);
+  };
+
+  React.useEffect(()=>{
+    type()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[currentType])
+
+  return (<>
+    <div>{text}<span>|</span></div>
+  </>)
+}
